025f2978363941411b2d650361b1ffd7943010b7
Steven Sequeira <ssequeira@uamv.edu.ni>
2026-07-16 18:47:42 -0600

    andikeepaccidentallylockingmyselfupoutofmydorminthemiddleofthenight

--- macro-zone/src/app/(tabs)/index.tsx
+++ macro-zone/src/app/(tabs)/index.tsx
@@ -1,16 +1,33 @@
-import { Text, ScrollView, StyleSheet, Platform } from "react-native";
-import { globalStyles } from "@/styles/global";
-import HomeHeader from "@/components/HomeHeader";
-import { Link } from "expo-router";
-import MacroGrid from "@/components/MacroGrid";
+import HomeHeader from '@/components/HomeHeader';
+import MacroGrid from '@/components/MacroGrid';
+import RecentMeals from '@/components/RecentMeals';
+import { getMeals, Meal } from '@/storage/meals';
+import { globalStyles } from '@/styles/global';
+import { useFocusEffect } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { ScrollView, Text } from 'react-native';
 
 export default function HomeScreen() {
-  return (
-   <ScrollView style = {globalStyles.container}>
-    <Text style ={globalStyles.title}>MacroZone</Text>
-    <HomeHeader/>
-    <MacroGrid/>
-   </ScrollView>
-  )
-}
+  const [meals, setMeals] = useState<Meal[]>([]);
 
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log('Loaded meals:', data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
+  return (
+    <ScrollView style={globalStyles.container}>
+      <Text style={globalStyles.title}>MacroZone</Text>
+      <HomeHeader />
+      <MacroGrid />
+      <RecentMeals />
+    </ScrollView>
+  );
+}
